1ed64d8b394773ca5c1e8b0d48b6a6f637210b57
Tuan Nguyen <tuanway@gmail.com>
2017-06-12 10:08:04 -0700

    add bridge.js

---
+++ dist/bridge.js
@@ -0,0 +1,68 @@
+var Bridge = new function() {
+    this.iFrame = null;
+    this.init = function(iFrame) {
+
+        this.iFrame = iFrame;
+
+        if (window.addEventListener) {
+            window.addEventListener("message", executeMethod, false);
+        } else if (window.attachEvent) {
+            window.attachEvent("onmessage", executeMethod);
+        }
+
+        function executeMethod(event) {
+            var method = event.data.method;
+            var callbackMethod = event.data.callbackMethod;
+            var params = event.data.params;
+
+            for (var i = 0; i < params.length; i++) {
+                if ((params[i] instanceof Object && params[i].hasOwnProperty("script"))) {
+                    params[i] = new Function("var message = {method: " + JSON.stringify(params[i]) + ", params: Array.prototype.slice.call(arguments)}; Bridge.sendMessageToClient(message);");
+                }
+            }
+
+            var functionToCall = eval("(" + method + ")");
+            try {
+                var retVal = functionToCall.apply(null, params);
+
+                if (callbackMethod) {
+                    Bridge.iFrame.contentWindow.postMessage({ method: callbackMethod, params: retVal }, '*');
+                }
+            } catch (e) {
+                console.log('Error calling function ' + method + ': ' + e.message);
+            }
+
+            function isFunction(stringFunction) {
+                try {
+                    return eval("(" + stringFunction + ")") instanceof Function;
+                } catch (error) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    this.sendMessageToClient = function(msg) {
+        function cloneAsObject(obj) {
+            if (obj === null || !(obj instanceof Object)) {
+                return obj;
+            }
+            var temp = (obj instanceof Array) ? [] : {};
+
+            for (var key in obj) {
+                temp[key] = cloneAsObject(obj[key]);
+            }
+            return temp;
+        }
+        
+        var params = JSON.stringify(cloneAsObject(msg.params)); 
+        msg.params = params;   
+        console.log(msg);     
+        try {
+            this.iFrame.contentWindow.postMessage(msg, '*');
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
+
+}
