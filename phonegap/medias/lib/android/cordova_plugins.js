417ab0b2eea5d4abb056cb1938cc1160c702f8bb
tuanway <tuan@atndev.com>
2021-03-18 16:34:25 -0500

    Add beep and vibration example

--- phonegap/medias/lib/android/cordova_plugins.js
+++ phonegap/medias/lib/android/cordova_plugins.js
@@ -409,6 +409,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-statusbar": "2.4.3",
+  "cordova-plugin-vibration": "3.1.1",
   "cordova-plugin-whitelist": "1.3.4",
   "cordova-plugin-app-version": "0.1.9",
   "cordova-plugin-enable-cleartext-traffic": "2.1.0",
